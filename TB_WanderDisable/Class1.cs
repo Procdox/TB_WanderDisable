75d716f5a6b0c5c7222fa08a015a25db0719815b
Andrew <blackledgecreates@gmail.com>
2021-09-27 14:15:50 -0700

    Initial commit

---
+++ TB_WanderDisable/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TB_WanderDisable
+{
+    public class Patcher
+    {
+        var harmony = new Harmony("com.example.patch");
+        harmony.PatchAll();
+    }
+    [BepInPlugin("org.bepinex.plugins.exampleplugin", "Example Plug-In", "1.0.0.0")]
+    public class ExamplePlugin : BaseUnityPlugin
+    {
+        void Awake()
+        {
+            UnityEngine.Debug.Log("Hello, world!");
+            MyPatcher.DoPatching();
+        }
+    }
+}
